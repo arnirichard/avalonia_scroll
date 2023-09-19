0ff64c332fdd3ab727ab78345f9f223f1beb0343
arnirichard <arni@arnirichard.dk>
2023-09-19 10:35:38 +0000

    Initial commit

---
+++ TestScroll/TestScroll/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestScroll.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public List<string> Items { get; } = new();
+
+    public MainViewModel()
+    {
+        Items.AddRange(Enumerable.Range(0, 100).Select(i => i.ToString()));
+    }
+}
---
+++ TestScroll/TestScroll/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace TestScroll.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
